e17189a17d4780befbc149b6c9703e5726678de3
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-12-12 22:30:02 +1300

    Select button elements

--- calculator.js
+++ calculator.js
@@ -1,3 +1,29 @@
+const inputDisplay = document.querySelector(".input-display");
+const resultDisplay = document.querySelector(".result-display");
+
+// Calculator buttons
+const clearButton = document.querySelector(".clear");
+
+const sevenButton = document.querySelector(".seven");
+const eightButton = document.querySelector(".eight");
+const nineButton = document.querySelector(".nine");
+const addButton = document.querySelector(".add");
+
+const fourButton = document.querySelector(".four");
+const fiveButton = document.querySelector(".five");
+const sixButton = document.querySelector(".six");
+const subtractButton = document.querySelector(".subtract");
+
+const oneButton = document.querySelector(".one");
+const twoButton = document.querySelector(".two");
+const threeButton = document.querySelector(".three");
+const multiplyButton = document.querySelector(".multiply");
+
+const pointButton = document.querySelector(".point");
+const zeroButton = document.querySelector(".zero");
+const equalsButton = document.querySelector(".equals");
+const divideButton = document.querySelector(".divide");
+
 function add(a, b) {
     return a + b;
 }
