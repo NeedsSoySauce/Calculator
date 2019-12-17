600585a311be242947f8c9b6289a4f655aa064a7
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-12-17 22:11:21 +1300

    Remove decimal points without trailing digits

--- calculator.js
+++ calculator.js
@@ -27,14 +27,6 @@ function operate(a, b, operator) {
     }
 }
 
-function isEquationValid(equation) {
-    let lastElement = equation[equation.length - 1];
-    if (lastElement in operator || equation.length === 0) {
-        return false;
-    }
-    return true;
-}
-
 function parseEquation(equation) {
     let parsed = equation.map(value => {
         if (!isNaN(value)) {
@@ -70,6 +62,114 @@ function evalulateEquation(equation) {
     })
 
     return temp[0];
+
+}
+
+function updateDisplay() {
+
+    let lastElement = equation[equation.length - 1];
+    if (lastElement !== undefined && lastElement.charAt(lastElement.length - 1) === ".") {
+        equation[equation.length - 1] = lastElement.slice(0, lastElement.length - 1);
+    }
+
+    if (equation.length !== 0) {
+        equationDisplay.innerText = equation.join(" ");
+    } else {
+        equationDisplay.innerText = "";
+    }
+
+    resultDisplay.innerText = "" + result;
+}
+
+function handleClick(event) {
+    let { target } = event;
+    let char = target.innerText;
+    let lastElement = equation[equation.length - 1];
+
+    if (target === clearButton) {
+        equation = []
+        result = 0;
+        resultCalculated = false;
+
+    } else if (target === clearEntryButton) {
+        if (resultCalculated) {
+            equation = [];
+        } else {
+            let lastElement = equation.pop();
+            if (lastElement !== undefined && lastElement.length > 1) {
+                equation.push(lastElement.slice(0, lastElement.length - 1));
+            }
+        }
+
+    } else if (target === equalsButton) {
+        if (equation.length === 0) {
+            equation = ["0"];
+        } else if (lastElement in operator) {
+            // Duplicate the last operand
+            let lastOperand = equation[equation.length - 2];
+            equation.push(lastOperand);
+        } else if (resultCalculated) {
+            // Repeat the last operation on the current result
+            equation = [result, ...equation.slice(equation.length - 3, equation.length - 1)]
+        }
+
+        result = evalulateEquation(parseEquation(equation));
+        equation.push(char)
+        resultCalculated = true;
+
+    } else if (target === pointButton) {
+        if (resultCalculated) {
+            // Start a new equation
+            equation = ["0" + char];
+            resultCalculated = false;
+        } else if (isNaN(lastElement)) {
+            // If the last element was an operator, start a new operand
+            equation.push("0" + char);
+        } else if (!lastElement.includes(char)) {
+            // We don't add a point if the last element already contains a point
+            equation[equation.length - 1] += char;
+        }
+
+    } else if (numberButtons.includes(target)) {
+        if (resultCalculated) {
+            // If the result of a calculation is currently being stored
+            // start a new equation
+            equation = [char];
+            resultCalculated = false;
+        } else if (!isNaN(lastElement)) {
+            // If a number was pressed and the last element was a number
+            // append this number to it
+            equation[equation.length - 1] += char;
+        } else {
+            equation.push(char);
+        }
+
+    } else if (operatorButtons.includes(target)) {
+        if (equation.length === 0) {
+            // If this operator is the first element being added, append a "0" before it
+            equation.push("0", char);
+        } else if (lastElement.charAt(lastElement.length - 1) === ".") {
+            // If the last char of the last element was a decimal point, remove the decimal point
+            // before pushing this operator
+            equation[equation.length - 1] = lastElement.slice(0, lastElement.length - 1);
+            equation.push(char);
+
+        } else if (lastElement in operator) {
+            // If an operator was pressed and the last element was an operator
+            // replace the last operator
+            equation[equation.length - 1] = char;
+        } else if (resultCalculated) {
+            // If the result of a calculation is currently being stored
+            // start a new equation with the result as the first operand
+            equation = [result, char];
+            resultCalculated = false;
+        } else {
+            equation.push(char)
+        }
+
+    } else {
+        console.log("Unhandled button: ", target);
+    }
 
 }
 
@@ -124,90 +224,7 @@ let result = 0;
 let resultCalculated = false;
 
 const buttonContainer = document.querySelector(".button-container");
-buttonContainer.addEventListener("click", ({ target }) => {
-    let char = target.innerText;
-    let lastElement = equation[equation.length - 1];
-
-    if (target === clearButton) {
-        equation = []
-        result = 0;
-        resultCalculated = false;
-
-    } else if (target === clearEntryButton) {
-        if (resultCalculated) {
-            equation = [];
-        } else {
-            let lastElement = equation.pop();
-            if (lastElement !== undefined && lastElement.length > 1) {
-                equation.push(lastElement.slice(0, lastElement.length - 1));
-            }
-        }
-
-    } else if (target === equalsButton) {
-        if (isEquationValid(equation)) {
-            if (resultCalculated) {
-                // Repeat the last operation on the current result
-                equation = [result, ...equation.slice(equation.length - 3, equation.length - 1)]
-            }
-            result = evalulateEquation(parseEquation(equation));
-            equation.push(char)
-            resultCalculated = true;
-        }
-
-    } else if (target === pointButton) {
-        if (resultCalculated) {
-            // Start a new equation
-            equation = ["0" + char];
-            resultCalculated = false;
-        } else if (isNaN(lastElement)) {
-            // If the last element was an operator, start a new operand
-            equation.push("0" + char);
-        } else if (!lastElement.includes(char)) {
-            // We don't add a point if the last element ends in a point
-            equation[equation.length - 1] += char;
-        }
-
-    } else if (numberButtons.includes(target)) {
-        if (resultCalculated) {
-            // If the result of a calculation is currently being stored
-            // start a new equation
-            equation = [char];
-            resultCalculated = false;
-        } else if (!isNaN(lastElement)) {
-            // If a number was pressed and the last element was a number
-            // append this number to it
-            equation[equation.length - 1] += char;
-        } else {
-            equation.push(char);
-        }
-
-    } else if (operatorButtons.includes(target)) {
-        if (lastElement in operator) {
-            // If an operator was pressed and the last element was an operator
-            // replace the last operator
-            equation[equation.length - 1] = char;
-        } else if (equation.length === 0) {
-            // If this operator is the first element being added, append a "0" before it
-            equation.push("0", char);
-        } else if (resultCalculated) {
-            // If the result of a calculation is currently being stored
-            // start a new equation with the result as the first operand
-            equation = [result, char];
-            resultCalculated = false;
-        } else {
-            equation.push(char)
-        }
-
-    } else {
-        console.log("Unhandled button: ", target);
-    }
-
-    if (equation.length !== 0) {
-        equationDisplay.innerText = equation.join(" ");
-    } else {
-        equationDisplay.innerText = "";
-    }
-
-    resultDisplay.innerText = "" + result;
-
-})
+buttonContainer.addEventListener("click", event => {
+    handleClick(event);
+    updateDisplay();
+});
